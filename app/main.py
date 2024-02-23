2fcf13c30b0966fb3d1d4ae3c2852516d324e6d8
下平大輝 <shimodairadaiki@shimodairadaiakiranoMacBook-Air.local>
2024-02-23 16:05:17 +0900

    etting dockerfile and docker-compose

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+from fastapi import FastAPI
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from os.path import join, dirname
 import os
+
+app = FastAPI()
 
 load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
@@ -15,13 +18,13 @@ def get_database():
     client = MongoClient(f'mongodb://{username}:{password}@{hostname}:27017/')
     return client['mydatabase']
 
-if __name__ == '__main__':
+@app.get("/get")
+def get_database_info():
     db = get_database()
-    
     collection_name = db['testCollection']
     insert_data = {"name":"John", "age":"999"}
     collection_name.insert_one(insert_data)
     
     print(list(collection_name.find()))
-    
+    return {"test":"ok!"}
     
